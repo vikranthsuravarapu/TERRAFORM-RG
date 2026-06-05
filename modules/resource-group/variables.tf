8b2f088c472cb444f78ef6963d9b9affa0d4554d
vikranth suravarapu <v-viksur@outlook.com>
2026-06-05 06:07:27 +0200

    comminting variables

--- modules/resource-group/variables.tf
+++ modules/resource-group/variables.tf
@@ -1,22 +1,27 @@
 variable "name" {
   description = "Name of the Resource Group"
   type        = string
+  default     = "rg-myproject-dev"
 }
 
 variable "location" {
-  description = "Azure region where the Resource Group will be created"
+  description = "Azure region"
   type        = string
-  
+  default     = "westeurope"
+}
+
+variable "environment" {
+  description = "Target environment"
+  type        = string
+  default     = "dev"
 }
 
 variable "tags" {
-  description = "Tags to apply to the Resource Group"
+  description = "Tags to apply to all resources"
   type        = map(string)
   default     = {
     environment = "dev"
     owner       = "vikram"
+    managed_by  = "terraform"
   }
 }
-
-
-
